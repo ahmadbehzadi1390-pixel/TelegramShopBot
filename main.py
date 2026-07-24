3c6d73998cffa0ef18d989551b729bdc5d6daae6
ahmadbehzadi1390-pixel <ahmadbehzadi1390@gmail.com>
2026-07-24 21:58:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,40 +1,23 @@
 import asyncio
-import os
 
-from aiohttp import web
 from aiogram import Bot, Dispatcher
 
 from config.settings import BOT_TOKEN
+
 from handlers.start import router as start_router
 from handlers.callbacks import router as callback_router
-
-
-async def health_check(request):
-    return web.Response(text="Bot is running")
-
-
-async def run_web_server():
-    app = web.Application()
-    app.router.add_get("/", health_check)
-
-    runner = web.AppRunner(app)
-    await runner.setup()
-
-    port = int(os.environ.get("PORT", 10000))
-    site = web.TCPSite(runner, "0.0.0.0", port)
-
-    await site.start()
+from handlers.stars import router as stars_router
 
 
 async def main():
-    await run_web_server()
-
     bot = Bot(token=BOT_TOKEN)
 
     dp = Dispatcher()
 
+    # ثبت همه روترها
     dp.include_router(start_router)
     dp.include_router(callback_router)
+    dp.include_router(stars_router)
 
     print("✅ Bot Started Successfully")
 
