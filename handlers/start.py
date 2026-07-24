c1fe9eb7458ecd58de79bd807c673935f7de4792
ahmadbehzadi1390-pixel <ahmadbehzadi1390@gmail.com>
2026-07-24 16:35:11 +0300

    Create start.py

---
+++ handlers/start.py
@@ -0,0 +1,12 @@
+from aiogram import Router
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+
+router = Router()
+
+@router.message(CommandStart())
+async def start_command(message: Message):
+    await message.answer(
+        "👋 سلام!\n\nبه ربات خدمات مجازی خوش اومدی."
+    )
+  
