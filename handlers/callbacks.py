30b972e33d48776ba9959ea1e2aa4ef32724570d
ahmadbehzadi1390-pixel <ahmadbehzadi1390@gmail.com>
2026-07-24 16:47:20 +0300

    Update callbacks.py

--- handlers/callbacks.py
+++ handlers/callbacks.py
@@ -1,12 +1,13 @@
-from aiogram import Router
+from aiogram import Router, F
 from aiogram.types import CallbackQuery
-from aiogram.filters import Text
 
 from keyboards.main_menu import main_menu
 from keyboards.special_menu import special_menu
 
 router = Router()
-@router.callback_query(Text("special_services"))
+
+
+@router.callback_query(F.data == "special_services")
 async def open_special_menu(callback: CallbackQuery):
     await callback.message.edit_reply_markup(
         reply_markup=special_menu
