c0eac9e11015ebcc99a58c9bbbb98debcf2740f4
ahmadbehzadi1390-pixel <ahmadbehzadi1390@gmail.com>
2026-07-24 20:02:15 +0300

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -2,11 +2,15 @@ from aiogram import Router
 from aiogram.filters import CommandStart
 from aiogram.types import Message
 
+from keyboards.main_menu import main_menu
+
 router = Router()
 
 @router.message(CommandStart())
 async def start_command(message: Message):
     await message.answer(
-        "👋 سلام!\n\nبه ربات خدمات مجازی خوش اومدی."
+        "👋 سلام!\n\n"
+        "به ربات خدمات مجازی خوش اومدی.\n\n"
+        "از منوی زیر سرویس مورد نظرت رو انتخاب کن 👇",
+        reply_markup=main_menu
     )
-  
