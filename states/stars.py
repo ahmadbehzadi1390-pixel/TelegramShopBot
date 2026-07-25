3e03ac5902248bd230d3027f91327bdc364fe2d2
ahmadbehzadi1390-pixel <ahmadbehzadi1390@gmail.com>
2026-07-25 18:23:06 +0300

    Update stars.py

--- states/stars.py
+++ states/stars.py
@@ -2,4 +2,6 @@ from aiogram.fsm.state import State, StatesGroup
 
 
 class StarsOrder(StatesGroup):
-    waiting_for_username = State()
+    waiting_for_user_id = State()
+    waiting_for_quantity = State()
+    waiting_for_confirm = State()
